96677cbebca8646d7baebcc1c1602f65968884de
anthonyjeamme <anthony@jeamme.fr>
2021-08-06 17:13:48 +0200

    test gamepad controller

--- src/io/useKeyboardAction.ts
+++ src/io/useKeyboardAction.ts
@@ -36,6 +36,38 @@ export const useKeyboardAction = () => {
     ACTION_GO_RIGHT: ["ArrowRight", "d"],
   }
 
+  const gamePadBinding = {
+    ACTION_GO_UP: [12],
+    ACTION_GO_DOWN: [13],
+    ACTION_GO_LEFT: [14],
+    ACTION_GO_RIGHT: [15],
+  }
+
+  const gamepadRef = useRef<number>(null)
+
+  const handleGamePadDown = e => {
+    console.log(e)
+  }
+
+  useEffect(() => {
+    window.addEventListener("gamepadconnected", function (e) {
+      console.log(
+        "Contrôleur n°%d connecté : %s. %d boutons, %d axes.",
+        e.gamepad.index,
+        e.gamepad.id,
+        e.gamepad.buttons.length,
+        e.gamepad.axes.length
+      )
+
+      gamepadRef.current = e.gamepad.index
+    })
+    window.addEventListener("gamepaddisconnected", e => {
+      if (gamepadRef.current === e.gamepad.index) {
+        gamepadRef.current = null
+      }
+    })
+  }, [])
+
   return {
     isActive: (action: TAction) => {
       const bind = keyBinding[action]
@@ -43,6 +75,32 @@ export const useKeyboardAction = () => {
       for (const key of bind) {
         if (keyMapper.getKey(key)) return true
       }
+
+      if (gamepadRef.current !== null) {
+        const gamepad = navigator.getGamepads()[gamepadRef.current]
+
+        const buttons = []
+
+        for (let i_button = 0; i_button < gamepad.buttons.length; i_button++) {
+          if (gamepad.buttons[i_button].pressed) {
+            buttons.push(i_button)
+          }
+        }
+
+        for (const but of buttons) {
+          if (gamePadBinding[action].includes(but)) return true
+        }
+
+        const [xaxis, yaxis] = gamepad.axes
+
+        if (xaxis > 0.2 && action === "ACTION_GO_RIGHT") return true
+        if (xaxis < -0.2 && action === "ACTION_GO_LEFT") return true
+        if (yaxis > 0.2 && action === "ACTION_GO_DOWN") return true
+        if (yaxis < -0.2 && action === "ACTION_GO_UP") return true
+
+        // console.log(gamepadRef.current)
+      }
+
       return false
     },
   }
