6f7a3eff6c46aebe8155943a9070ad250aa51896
anthonyjeamme <anthony@jeamme.fr>
2021-08-08 19:41:39 +0200

    ambiant color ok

--- src/components/Game/Game.tsx
+++ src/components/Game/Game.tsx
@@ -17,6 +17,73 @@ export const isBrowser = () => typeof window !== "undefined"
 
 const ZOOM = 2
 
+const gradient = [
+  {
+    position: 0.1,
+    color: [28, 26, 64],
+  },
+  {
+    position: 0.18,
+    color: [58, 67, 120],
+  },
+
+  {
+    position: 0.2,
+    color: [92, 106, 151],
+  },
+
+  {
+    position: 0.23,
+    color: [191, 190, 186],
+  },
+
+  {
+    position: 0.5,
+    color: [204, 204, 204],
+  },
+  {
+    position: 0.82,
+    color: [193, 190, 184],
+  },
+
+  {
+    position: 0.9,
+    color: [58, 67, 120],
+  },
+  {
+    position: 1,
+    color: [28, 26, 64],
+  },
+]
+
+const getBetweenNumber = (a, b, ratio) => a + (b - a) * ratio
+const getBetweenColor = ([r1, g1, b1], [r2, g2, b2], ratio) => [
+  Math.round(getBetweenNumber(r1, r2, ratio)),
+  Math.round(getBetweenNumber(g1, g2, ratio)),
+  Math.round(getBetweenNumber(b1, b2, ratio)),
+]
+
+const getColorFromGradient = (gradient, position) => {
+  if (position < 0 || position > 1) throw `Out of gradient range`
+
+  if (position <= gradient[0].position) return `rgba(${gradient[0].color})`
+
+  for (let i = 0; i < gradient.length; i++) {
+    const step = gradient[i]
+
+    if (position <= step.position) {
+      const previousStep = gradient[i - 1]
+      const nextStep = step
+
+      const ratio =
+        (position - previousStep.position) /
+        (nextStep.position - previousStep.position)
+
+      return getBetweenColor(previousStep.color, nextStep.color, ratio)
+    }
+  }
+}
+
 const Game = () => {
   const canvasRef = useRef<HTMLCanvasElement>()
   const shadowCanvasRef = useRef<HTMLCanvasElement>()
@@ -174,14 +241,15 @@ const Game = () => {
     const shadowCtx = shadowCanvasRef.current.getContext("2d")
 
     const getDayColor = () => {
-      const h = timeRef.current.h
-      if (h > 9 && h < 20) return "#c9c8c6"
+      const position =
+        new Date().getTime() / 1000 / 60 -
+        Math.floor(new Date().getTime() / 1000 / 60)
 
-      if (h > 8 && h < 21) return "#777d95"
+      const ambianceColor = getColorFromGradient(gradient, position)
 
-      if (h > 7 && h < 22) return "#303566"
+      console.log(ambianceColor)
 
-      return "#1c1a40"
+      return `rgba(${ambianceColor.join(",")})`
     }
 
     shadowCtx.fillStyle = getDayColor()
